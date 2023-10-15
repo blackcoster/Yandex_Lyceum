7e74a1fdbcf4552e4c0ba5a8fe4060e6c553893d
polina <krupenina.p@mail.ru>
2023-10-15 12:08:08 +0300

    4

---
+++ lyceum21/4/1.py
@@ -0,0 +1,30 @@
+print('polina')
+# a = input('Type your surname')
+
+x = 5.8
+x = 6.5
+y = round(x)
+# print(round(x))
+
+
+# print(round(2.458,1))
+print(round(2.8499999, 1))
+print(round(2.85, 1))  # думает что 2.8500001
+
+round(5.8)
+
+# word = input('Type word: ')
+# length = len(word)
+# print(f'Вы ввели слово длиной {length} символов')
+
+print(len('Hello, world!'))
+
+print(min(6, 2, 3, 4, 5, 6))
+print(max(4.7, 8.9, 5.5))
+print(min('aa','ba','ca'))
+print(min('aa','Aa'))
+print(min('1','a'))
+print(min('aa','BB'))
+print('polina'>'Taya')
+# print(min('rt',3))# нельзя
+
---
+++ lyceum21/4/turt.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+
+paul = Turtle()
+
+paul.pu()
+# paul.penup() same!
+paul.goto(-100,0)
+paul.pd()
+paul.goto(0,100)
+paul.goto(100,0)
+paul.color('royalblue')
+paul.goto(0,-100)
+paul.goto(-100,0)
+done()
+
