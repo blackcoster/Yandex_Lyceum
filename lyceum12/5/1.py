75436b0f1015d007cbc544d4e5a1fd448cdb4cc3
polina <krupenina.p@mail.ru>
2023-10-18 19:59:17 +0300

    5

---
+++ lyceum12/5/1.py
@@ -0,0 +1,44 @@
+num1 = 5
+num2 = 6
+num3 = num1 + num2
+print(num1+num2)
+
+# + - * /
+a = 3
+b = 3
+
+
+
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b) # // %
+
+x = 0
+y = 10
+z = y - 10
+y = 2 * z
+x = z - 10
+print(x, y, z)
+
+s = 3
+t = 4
+u = s + t
+s = u + 1
+t = s
+u = s - t
+print(u)
+
+print(123 + 123)
+print('123'+'123')
+# print(123'123')
+print('123+123')
+
+a = 7
+print(a,'a')
+
+name = 'polina'
+print('my name is',name)
+print('my name is '+name)
+print(f'my name is {name}')
+print('my name is {name}')
---
+++ lyceum12/5/2.py
@@ -0,0 +1,19 @@
+year_of_birth = '2000'
+age = 2023 - int(year_of_birth)
+print(f'you are {age} years old')
+
+print(int(60.1))
+print(str(67.9))
+a = 772913
+tip = type(a)
+print(tip)
+
+# print('type')
+year_of_birth = input('type year of birth: ')
+
+age = 2023 - int(year_of_birth)
+print(f'you are {age} years old')
+
+
+
+
