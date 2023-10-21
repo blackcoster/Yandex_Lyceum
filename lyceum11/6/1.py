5f387d1bc79b09fc58f41305418c595214f2719f
polina <krupenina.p@mail.ru>
2023-10-21 15:22:34 +0300

    6

---
+++ lyceum11/6/1.py
@@ -0,0 +1,33 @@
+print(round((0.1 + 0.2), 1))
+a = 1
+b = 2
+f = 7 * (a + b) + b + (a * a) * b
+
+f = lambda a, b: 7 * (a + b) + b + (a * a) * b
+
+print(f(1, 2))
+
+a = 5 > 0
+b = 6 == 7
+print(a and b)
+
+# a = int(input())
+# b = int(inpu())
+print(type(a))
+print(a + b)
+
+a = '6'
+print(a * (-3))
+
+print('привет ' * 500)
+# a = ''
+# a = ' '
+
+
+one_hundred = 100
+print(one_hundred)
+
+a = 5
+b = 5
+c = 10
+print(f'{a} + {b} = {c}')
