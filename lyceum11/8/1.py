8104e7d3543e8e69d1c9d5ce2afda2eb91b3ad33
polina <krupenina.p@mail.ru>
2023-10-28 14:38:26 +0300

    8

---
+++ lyceum11/8/1.py
@@ -0,0 +1,37 @@
+# num = 12
+# last_digit = num % 10
+# first_digit = num // 10
+# print(first_digit, last_digit)
+
+
+# num = 781
+# first_digit = num // 100
+# # center_digit = num // 10 % 10
+# center_digit = num % 100 // 10
+# last_digit = num % 10
+# print(first_digit, center_digit, last_digit)
+
+
+num = 12_345
+a = num // 10000
+b = num % 10000 // 1000
+c = num % 1000 // 100
+d = num % 100 // 10
+e = num % 10
+
+print(a,b,c,d,e)
+
+
+# a = int(input())
+# print(a+5)
+print(150//60,150%60)
+
+print(a,b,c,sep='')
+
+# 10/3 = 3.3333
+# 10//3 = 3
+# 10%3 = 1
+
+
+a = int('123')
+print(a*2)
