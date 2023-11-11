8b9714baece9f179c27b1968f73c569c23bf6178
polina <krupenina.p@mail.ru>
2023-11-11 15:11:34 +0300

    10

--- lyceum21/10/dz1.py
+++ lyceum21/10/dz1.py
@@ -38,19 +38,3 @@
 # print(name)
 
 
-hours = int(input())
-minutes = int(input())
-if (hours == 10 or hours == 11) and minutes == 30:
-    print('Да')
-else:
-    print('Нет')
-
-
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if (x1 + y1) % 2 == (x2+y2) & 2:
-    print('Да')
-else:
-    print('Нет')
