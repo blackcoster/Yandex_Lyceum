b462914a6549cbe08c68b6f039c01280be90c22d
polina <krupenina.p@mail.ru>
2023-11-11 15:15:26 +0300

    12

--- lyceum11/12/1.py
+++ lyceum11/12/1.py
@@ -93,7 +93,7 @@ x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
-if (x1 + y1) % 2 == (x2+y2) & 2:
+if (x1 + y1) % 2 == (x2+y2) % 2:
     print('Да')
 else:
     print('Нет')
