400bae6d7e26b0adfb4c78b2784cdf0160c914ba
polina <krupenina.p@mail.ru>
2023-11-23 17:55:40 +0300

    14

--- lyceum21/14/test.py
+++ lyceum21/14/test.py
@@ -1,7 +1,7 @@
 # task 1
 n = int(input())
-if n % 3 == 0 and n % 7 == 0: # числа в условиях могут отличаться
-    print('...') # принты вписать самостоятельно
+if n % 3 == 0 and n % 7 == 0:  # числа в условиях могут отличаться
+    print('...')  # принты вписать самостоятельно
 elif n % 3 == 0:
     print('...')
 elif n % 7 == 0:
@@ -9,21 +9,20 @@ elif n % 7 == 0:
 else:
     print('...')
 
-
-#task 2
+# task 2
 
 stroka = input()
 count = 0
 dlina = 0
-while stroka != 'СЕКРЕТ': # стоп-слово может быть другим
-    if 'цвет' in stroka or 'капуст' in stroka: #слова для поиска могут быть другими
-        count+=1
+while stroka != 'СЕКРЕТ':  # стоп-слово может быть другим
+    if 'цвет' in stroka or 'капуст' in stroka:  # слова для поиска могут быть другими
+        count += 1
         dlina += len(stroka)
     stroka = input()
-if count==0:
+if count == 0:
     print(count)
 else:
-    print(count, round(dlina/count,3))
+    print(count, round(dlina / count, 3))  # количество знаков после запятой может отличчаться
 
 # task 3
 
@@ -32,9 +31,9 @@ for i in range(n):
     start = int(input())
     stop = int(input())
     step = int(input())
-    if start>stop or step<=0:
+    if start > stop or step <= 0:
         print('ОШИБКА')
         break
-    for j in range(start,stop+1,step):
-        print(j,end = ' ')
+    for j in range(start, stop + 1, step):
+        print(j, end=' ')
     print()
