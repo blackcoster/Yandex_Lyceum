64ff6742913ba0a8fcce340df98ded3688bbfdb7
polina <krupenina.p@mail.ru>
2023-12-13 19:38:25 +0300

    16

--- lyceum11/16/2.py
+++ lyceum11/16/2.py
@@ -14,7 +14,7 @@
 
 # for i in range(5,1-1,-1):
 #     print(i)
-
+ 
 a = 5
 for i in range(a+1): #[0 1 2 3 4 5]
     print(a-i)       #[5 4 3 2 1 0]
---
+++ lyceum12/16/1.py
@@ -0,0 +1,20 @@
+# for _ in range(3): #[0 1 2]
+#     print('hello')
+#     print(1)
+# print('all')
+#
+# for i in range(100):
+#     print('krutitsa')
+# print('sbor')
+
+# for i in range(3): #[0 1 2]
+#     a = int(input('type number - '))
+#     print(a*a)
+#
+
+for i in range(10):  #[0 ..9]
+    print(i+1)
+
+
+for i in range(5):
+    print('cup',i+1)
