a204b4971766ea2a6dabb88f0baa55fe7beb258a
polina <krupenina.p@mail.ru>
2023-12-13 18:28:11 +0300

    16

---
+++ lyceum11/16/1.py
@@ -0,0 +1,28 @@
+# for i in range(3):
+#     print('привет')
+#
+
+for i in range(5):     # i = [0,1,2,3,4]
+    print('polina')
+print(3)
+for i in range(5):     # i = [0,1,2,3,4]
+    print('polina')
+print(3)
+for i in range(5):     # i = [0,1,2,3,4]
+    print('polina')
+print(3)
+
+
+
+# range(finish) [0..finish-1]  range(7) = [0 1 2 3 4 5 6]
+# range(start,finish) [start..finish-1]  range(3,7) = [3 4 5 6]
+# range(start,finish,step) [start.(+step).finish-1]  range(3,8,2) = [3 5 7]
+a = 10
+while a>=0:
+    a = a-3
+    print(a)
+
+for i in range(100):
+    print('krutiza')
+    
+print('sbor')
---
+++ lyceum11/16/2.py
@@ -0,0 +1,20 @@
+
+# for i in range(3):
+#     a = int(input('type number - '))
+#     print(a*a)
+#
+# for i in range(10): #[0 1 2 3 4 5 6 7 8 9]
+#     print(i, 'person')
+#
+# for i in range(10): #[0 1 2 3 4 5 6 7 8 9]
+#     print(i+1, 'person')
+#
+# for i in range(1,11): #[1 2 3 4 5 6 7 8 9 10]
+#     print(i, 'person')
+
+# for i in range(5,1-1,-1):
+#     print(i)
+
+a = 5
+for i in range(a+1): #[0 1 2 3 4 5]
+    print(a-i)       #[5 4 3 2 1 0]
