18b7f53aa346a0e2974ab2968fd37d0bdd8cafab
polina <krupenina.p@mail.ru>
2023-12-16 16:55:03 +0300

    17

---
+++ lyceum12/17/1.py
@@ -0,0 +1,42 @@
+# s = 0
+# n = int(input())
+# for i in range(1,n+1): # 1.. n
+#     s = s+i
+# print(s)
+#
+# s = 0
+# for i in range(10):
+#     ans = input()
+#     if ans=='Да':
+#         s+=1
+# print(s)
+#
+# for i in range(5):
+#     name = input()
+#     print(i+1,name)
+
+for i in range(5):
+    name = input()
+    print(i+1,name)
+
+for i in range(10):
+    print('*'*(i+1))
+
+for i in range(1,10):
+    print(str(i)*i)
+
+n = int(input())
+for i in range(n):
+    print('*' * n)
+
+s = input()
+n = int(input())
+for i in range(n):
+    print(s * n)
+
+symbol = input()
+m = int(input())
+n = int(input())
+
+for i in range(n):
+    print(symbol * m)
