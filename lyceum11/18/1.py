469d8ea63f26480f2e6bb53c339844c33d144a8c
polina <krupenina.p@mail.ru>
2023-12-23 18:52:11 +0300

    18

---
+++ lyceum11/18/1.py
@@ -0,0 +1,65 @@
+# m = int(input()) #3
+# n = int(input()) #9
+#
+# if m<n:
+#     for i in range(m,n+1):
+#         print(i)
+# else:
+#     for i in range(m,n-1,-1):
+#         print(i)
+
+
+# count = 0
+# total = 0
+# for i in range(5):
+#     n = int(input())
+#     if n > 0:
+#         total = total+n
+#         count = count+1
+# average = total/count
+# print(average)
+
+
+# total = 1
+# for i in range(5):
+#     n = int(input())
+#     total = total * n
+# print(total)
+
+# boy = input('type girls name ' )
+# girl = input('type boy name ' )
+boy = 'masha'
+girl = 'dima'
+# temp = boy
+#
+# boy = girl # boy = dima, girl = girl temp = masha
+# girl = boy
+# girl = temp # boy = dima, girl = masha
+boy,girl = girl, boy
+
+print(boy,girl)
+
+
+num = 11
+flag = True
+for i in range(2,num):
+    if num % i==0:
+        flag = False
+
+if flag==True
+if flag
+# a = float('inf')
+
+minimum = 2
+for i in range(2):
+    n = float(input('введите число от 0 до 1 - '))
+    if n < minimum:
+        minimum = n
+print(minimum)
+
+minimum = float(input('введите число от 0 до 1 - '))
+for i in range(2):
+    n = float(input('введите число от 0 до 1 - '))
+    if n < minimum:
+        minimum = n
+print(minimum)
