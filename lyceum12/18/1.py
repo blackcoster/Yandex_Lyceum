51026459b07c2c9bd73250cf03a190f69a55b2a7
polina <krupenina.p@mail.ru>
2023-12-30 17:03:43 +0300

    18

---
+++ lyceum12/18/1.py
@@ -0,0 +1,14 @@
+# num = int(input())
+# while num != 0:
+#     print(num**2)
+#     num = int(input())
+#
+# for i in range(0):
+#     print()
+#
+
+num = 1984
+while num != 0:
+    digit = num % 10
+    print(digit)
+    num = num//10
